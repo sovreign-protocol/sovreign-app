9c70dca7d3053600a8358a04a95fe020ce4e9cd0
Jeff Reiner <theundeadghost@gmail.com>
2021-08-04 14:19:20 +0200

    Update useReignStaked.ts

--- hooks/view/useReignStaked.ts
+++ hooks/view/useReignStaked.ts
@@ -2,17 +2,24 @@ import type { BigNumber } from "@ethersproject/bignumber";
 import type { Contract } from "@ethersproject/contracts";
 import useSWR from "swr";
 import useReignFacet from "../contracts/useReignFacet";
+import useWeb3Store from "../useWeb3Store";
 
-const getReignStaked = (contract: Contract) => async () => {
-  const value: BigNumber = await contract.reignStaked();
+const getReignStaked =
+  (contract: Contract) => async (_: string, user: string) => {
+    const value: BigNumber = await contract.balanceOf(user);
 
-  return value;
-};
+    return value;
+  };
 
 export default function useReignStaked() {
+  const account = useWeb3Store((state) => state.account);
+
   const contract = useReignFacet();
 
-  const shouldFetch = !!contract;
+  const shouldFetch = !!contract && typeof account === "string";
 
-  return useSWR(shouldFetch ? ["ReignStaked"] : null, getReignStaked(contract));
+  return useSWR(
+    shouldFetch ? ["ReignStaked", account] : null,
+    getReignStaked(contract)
+  );
 }
