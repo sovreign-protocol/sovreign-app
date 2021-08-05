6ba24c99949f05e98a8d958f9a63bf7ccd44e7b8
Jeff Reiner <theundeadghost@gmail.com>
2021-08-05 14:22:01 +0200

    Buttons?

--- components/harvest/sovHarvest.tsx
+++ components/harvest/sovHarvest.tsx
@@ -2,6 +2,7 @@ import useWeb3Store from "@/hooks/useWeb3Store";
 import { useEpochStartWrappingRewards } from "@/hooks/view/useEpochStart";
 import { useUserRewardsWrappingRewards } from "@/hooks/view/useUserRewards";
 import getTimeUntilNextEpoch from "@/utils/getTimeUntilNextEpoch";
+import classNames from "classnames";
 
 export default function SOVHarvest() {
   const account = useWeb3Store((state) => state.account);
@@ -20,6 +21,18 @@ export default function SOVHarvest() {
         <p>Next epoch in {getTimeUntilNextEpoch(epochStart)}</p>
 
         <p>{userRewards?.toString() ?? "Error Fetching Rewards"}</p>
+
+        <div className="space-y-2">
+          <button
+            className={classNames(
+              "px-4 py-2 w-full rounded-md font-medium focus:outline-none focus:ring-4",
+              false ? "bg-white text-primary" : "bg-primary-300"
+            )}
+            disabled
+          >
+            Harvest Rewards
+          </button>
+        </div>
       </div>
     </div>
   );
