c84b220c6cfbd0e779cd47b67bffbb19ce6e43ea
Jeff Reiner <theundeadghost@gmail.com>
2021-08-06 18:02:23 +0200

    Ship initial token adjustmnet

--- components/rebalance/allocationAdjustment.tsx
+++ components/rebalance/allocationAdjustment.tsx
@@ -1,12 +1,20 @@
+import useBasketBalancer from "@/hooks/contracts/useBasketBalancer";
+import useContinuousTokenAllocation from "@/hooks/view/useContinuousTokenAllocation";
 import useHasVotedInEpoch from "@/hooks/view/useHasVotedInEpoch";
 import useMaxDelta from "@/hooks/view/useMaxDelta";
 import useTokenAllocation from "@/hooks/view/useTokenAllocation";
+import { TransactionResponse } from "@ethersproject/providers";
+import { parseUnits } from "@ethersproject/units";
 import classNames from "classnames";
 import { useEffect, useState } from "react";
+import { Minus, Plus } from "react-feather";
 import VotingPower from "./votingPower";
 
 export default function AllocationAdjustment() {
   const { data: tokenAllocation } = useTokenAllocation();
+
+  const { mutate: continuousTokenAllocationMutate } =
+    useContinuousTokenAllocation();
 
   const totalAllocation =
     tokenAllocation &&
@@ -14,21 +22,49 @@ export default function AllocationAdjustment() {
       .map((token) => token.allocation)
       .reduce((prev, cur) => cur + prev);
 
-  const [inputObject, inputObjectSet] = useState<Record<number, number>>();
+  const [inputObject, inputObjectSet] = useState<Record<string, number>>();
 
   useEffect(() => {
     if (typeof tokenAllocation === "undefined") {
       return;
     }
 
-    inputObjectSet({
-      ...tokenAllocation.map((el) =>
-        parseFloat(((el.allocation / totalAllocation) * 100).toFixed(2))
-      ),
-    });
+    inputObjectSet(
+      Object.fromEntries(
+        tokenAllocation.map((el) => [el.address, el.allocation])
+      )
+    );
   }, [tokenAllocation]);
 
-  const { data: hasVotedInEpoch } = useHasVotedInEpoch();
+  console.log(inputObject);
+
+  const { data: hasVotedInEpoch, mutate: hasVotedInEpochMutate } =
+    useHasVotedInEpoch();
+
+  const { data: delta } = useMaxDelta();
+
+  const total =
+    inputObject && Object.values(inputObject).reduce((prev, cur) => prev + cur);
+
+  const canUpdate = total === totalAllocation;
+
+  const basketBalancer = useBasketBalancer();
+
+  async function castAdjustedVote() {
+    try {
+      const tx: TransactionResponse = await basketBalancer.updateAllocationVote(
+        Object.keys(inputObject),
+        Object.values(inputObject).map((el) => parseUnits(el.toString()))
+      );
+
+      await tx.wait();
+
+      hasVotedInEpochMutate();
+      continuousTokenAllocationMutate();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div>
@@ -38,26 +74,83 @@ export default function AllocationAdjustment() {
         </div>
 
         <div className="flex-1">
-          <div className="mb-4">
-            <ul className="space-y-4">
-              {tokenAllocation?.map((token, tokenIndex) => (
-                <TokenAdjust
-                  key={tokenIndex}
-                  token={token}
-                  totalAllocation={totalAllocation}
-                />
+          <ul className="space-y-4 mb-4">
+            {inputObject &&
+              tokenAllocation &&
+              tokenAllocation?.map((token) => (
+                <li key={token.address}>
+                  <div className="mb-2">
+                    <p className="font-medium leading-5">{token.symbol}</p>
+                  </div>
+
+                  <div className="flex items-start justify-between">
+                    <div>
+                      <p>
+                        Current Allocation: <span>{token.allocation}</span>
+                      </p>
+
+                      <p>
+                        New Allocation:{" "}
+                        <span>{inputObject[token.address]}</span>
+                      </p>
+                    </div>
+
+                    <div className="flex divide-x">
+                      <button
+                        className="flex-1 p-2 border-primary-300 rounded-l-md whitespace-nowrap bg-primary flex focus:outline-none focus:ring-4"
+                        onClick={() =>
+                          inputObjectSet((prev) => {
+                            const prevValue = prev[token.address];
+
+                            if (prevValue >= token.allocation + delta) {
+                              return prev;
+                            }
+
+                            return {
+                              ...prev,
+                              [token.address]: prevValue + 0.5,
+                            };
+                          })
+                        }
+                      >
+                        <Plus size={20} />
+                      </button>
+
+                      <button
+                        className="flex-1 p-2 border-primary-300 rounded-r-md whitespace-nowrap bg-primary flex focus:outline-none focus:ring-4"
+                        onClick={() =>
+                          inputObjectSet((prev) => {
+                            const prevValue = prev[token.address];
+
+                            if (prevValue <= token.allocation - delta) {
+                              return prev;
+                            }
+
+                            return {
+                              ...prev,
+                              [token.address]: prevValue - 0.5,
+                            };
+                          })
+                        }
+                      >
+                        <Minus size={20} />
+                      </button>
+                    </div>
+                  </div>
+                </li>
               ))}
-            </ul>
-          </div>
+          </ul>
 
           <div className="space-y-2">
             <button
-              type="submit"
+              onClick={castAdjustedVote}
               className={classNames(
                 "px-4 py-2 w-full rounded-md font-medium focus:outline-none focus:ring-4",
-                !hasVotedInEpoch ? "bg-white text-primary" : "bg-primary-300"
+                canUpdate && !hasVotedInEpoch
+                  ? "bg-white text-primary"
+                  : "bg-primary-300"
               )}
-              disabled={hasVotedInEpoch}
+              disabled={!canUpdate || hasVotedInEpoch}
             >
               Cast Vote
             </button>
@@ -66,67 +159,4 @@ export default function AllocationAdjustment() {
       </div>
     </div>
   );
-}
-
-function TokenAdjust({ token, totalAllocation }) {
-  {
-    const [value, valueSet] = useState<number>(token.allocation);
-
-    const { data: delta } = useMaxDelta();
-
-    return (
-      <li>
-        <div className="flex justify-between mb-2">
-          <p>{token.symbol}</p>
-        </div>
-
-        <div className="flex justify-between mb-2">
-          <p>
-            Current: <span>{value}</span>
-          </p>
-
-          <button
-            onClick={() =>
-              valueSet((prev) => {
-                if (prev >= token.allocation + delta) return prev;
-
-                return prev + 0.5;
-              })
-            }
-            className="h-8 w-12 bg-primary"
-          >
-            +
-          </button>
-
-          <button
-            onClick={() =>
-              valueSet((prev) => {
-                if (prev <= token.allocation - delta) return prev;
-
-                return prev - 0.5;
-              })
-            }
-            className="h-8 w-12 bg-primary"
-          >
-            -
-          </button>
-
-          {/* <p>
-            New: <span>{inputObject?.[tokenIndex]}</span>
-          </p> */}
-        </div>
-
-        {/* <div className="flex">
-          <input
-            value={inputObject?.[tokenIndex]}
-            onChange={handleOnChange}
-            className="flex-1"
-            type="range"
-            min={0}
-            max={100}
-          />
-        </div> */}
-      </li>
-    );
-  }
 }
