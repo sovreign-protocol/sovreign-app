55f0b8927074b903970f6f7fd926d134891a7698
Jeff Reiner <theundeadghost@gmail.com>
2021-08-10 15:32:00 +0200

    Update network.tsx

--- components/network.tsx
+++ components/network.tsx
@@ -18,7 +18,7 @@ export default function NetworkIndicator() {
   }
 
   return (
-    <div className="px-4 py-3 bg-primary-400 ring-1 ring-inset ring-network-rinkeby text-network-rinkeby text-sm rounded-md">
+    <div className="fixed transform -translate-x-1/2 left-1/2 bottom-4 sm:relative sm:transform-none sm:bottom-auto sm:left-auto px-6 sm:px-4 py-3 bg-primary-400 ring-1 ring-inset ring-network-rinkeby text-network-rinkeby text-sm rounded-full sm:rounded-md z-50 sm:z-auto">
       {info.label}
     </div>
   );
