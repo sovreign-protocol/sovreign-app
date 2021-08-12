7a4db485ab945e68530fef23e4dc7171a68773bb
Jeff Reiner <theundeadghost@gmail.com>
2021-08-12 17:28:19 +0200

    Update harvest.tsx

--- pages/harvest.tsx
+++ pages/harvest.tsx
@@ -3,10 +3,23 @@ import GovRewardsHarvest from "@/components/harvest/govRewardsHarvest";
 import LPRewardsHarvest from "@/components/harvest/lpRewardsHarvest";
 import WrappingRewardsHarvest from "@/components/harvest/wrappingRewardsHarvest";
 import { CONTRACT_ADDRESSES } from "@/constants";
-import useWeb3Store from "@/hooks/useWeb3Store";
+import useWeb3Store, { State } from "@/hooks/useWeb3Store";
+import { useMemo } from "react";
+
+const selector = (state: State) => state.chainId;
 
 function HarvestPage() {
-  const chainId = useWeb3Store((state) => state.chainId);
+  const chainId = useWeb3Store(selector);
+
+  const LPRewardsREIGNWETH = useMemo<string>(
+    () => CONTRACT_ADDRESSES.LPRewardsREIGNWETH[chainId],
+    [chainId]
+  );
+
+  const LPRewardsSOVUSDC = useMemo<string>(
+    () => CONTRACT_ADDRESSES.LPRewardsSOVUSDC[chainId],
+    [chainId]
+  );
 
   return (
     <section className="pt-8 md:pt-16 pb-8">
@@ -21,12 +34,12 @@ function HarvestPage() {
           <GovRewardsHarvest />
 
           <LPRewardsHarvest
-            contractAddress={CONTRACT_ADDRESSES.LPRewardsREIGNWETH[chainId]}
+            contractAddress={LPRewardsREIGNWETH}
             title="REIGN/WETH Pool Rewards"
           />
 
           <LPRewardsHarvest
-            contractAddress={CONTRACT_ADDRESSES.LPRewardsSOVUSDC[chainId]}
+            contractAddress={LPRewardsSOVUSDC}
             title="SOV/USDC Pool Rewards"
           />
         </div>
