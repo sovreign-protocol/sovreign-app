ab8d8703ea3b48ae6984a4f63f6870046c35f04b
Jeff Reiner <theundeadghost@gmail.com>
2021-08-17 17:17:50 +0200

    Update bn.ts

--- utils/bn.ts
+++ utils/bn.ts
@@ -1,5 +1,6 @@
 import type { BigNumberish } from "@ethersproject/bignumber";
 import { formatUnits } from "@ethersproject/units";
+import { BigNumber } from "@ethersproject/bignumber";
 
 export default function formatToFloat(value: BigNumberish, decimals = 18) {
   if (typeof value === "undefined") {
@@ -7,4 +8,30 @@ export default function formatToFloat(value: BigNumberish, decimals = 18) {
   }
 
   return parseFloat(formatUnits(value, decimals));
+}
+
+const ZERO = BigNumber.from(0);
+
+const MAX_UINT_256 = BigNumber.from(2).pow(256).sub(1);
+
+export function scaleBy(
+  value: BigNumber,
+  decimals?: number
+): BigNumber | undefined {
+  if (decimals === undefined) {
+    return undefined;
+  }
+
+  return value.mul(10 ** decimals);
+}
+
+export function unscaleBy(
+  value: BigNumber,
+  decimals?: number
+): BigNumber | undefined {
+  if (decimals === undefined) {
+    return undefined;
+  }
+
+  return value.div(10 ** decimals);
 }
