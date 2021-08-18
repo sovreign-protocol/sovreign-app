037d9f4024dfe9951142a88d26163669db7cdb59
Jeff Reiner <theundeadghost@gmail.com>
2021-08-18 15:03:45 +0200

    Update network.tsx

--- components/network.tsx
+++ components/network.tsx
@@ -18,7 +18,7 @@ export default function NetworkIndicator() {
   }
 
   return (
-    <div className="fixed transform -translate-x-1/2 left-1/2 bottom-4 md:relative md:transform-none md:bottom-auto md:left-auto px-6 md:px-4 py-3 bg-primary-400 ring-1 ring-inset ring-network-rinkeby text-network-rinkeby text-sm rounded-full md:rounded-md z-50 md:z-auto">
+    <div className="fixed transform -translate-x-1/2 left-1/2 bottom-4 md:relative md:transform-none md:bottom-auto md:left-auto px-6 md:px-4 py-3 bg-primary-400 md:bg-network-rinkeby/[0.10] ring-1 ring-inset ring-network-rinkeby text-network-rinkeby text-sm rounded-full md:rounded-xl z-50 md:z-auto">
       {info.label}
     </div>
   );
