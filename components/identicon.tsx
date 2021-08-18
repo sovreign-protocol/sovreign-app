57f26bb7a36714a1a6ac905d838773f05266c022
Jeff Reiner <theundeadghost@gmail.com>
2021-08-18 18:15:40 +0200

    Shave off 15kb

--- components/identicon.tsx
+++ components/identicon.tsx
@@ -1,15 +1,33 @@
-import Jazzicon from "@metamask/jazzicon";
 import { useEffect, useRef } from "react";
 
 export default function Identicon({ address }: { address: string }) {
+  const jazzicon = useRef<(diameter: number, seed: number) => HTMLDivElement>();
+
   const ref = useRef<HTMLDivElement>();
 
   useEffect(() => {
+    async function loadJazzicon() {
+      const generateIdenticon = (await import("@metamask/jazzicon")).default;
+
+      jazzicon.current = generateIdenticon;
+    }
+
+    loadJazzicon();
+  }, []);
+
+  useEffect(() => {
+    if (typeof jazzicon.current === "undefined") {
+      return;
+    }
+
     if (address && ref.current) {
       ref.current.innerHTML = "";
-      ref.current.appendChild(Jazzicon(20, parseInt(address.slice(2, 10), 16)));
+
+      ref.current.appendChild(
+        jazzicon.current(20, parseInt(address.slice(2, 10), 16))
+      );
     }
-  }, [address]);
+  }, [jazzicon.current, address]);
 
   return <div className="h-5 w-5 rounded-full bg-primary-400" ref={ref} />;
 }
