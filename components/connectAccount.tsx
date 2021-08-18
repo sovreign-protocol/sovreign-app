37b5373f745c73d5ad9a2f9f925f6a0767a39f13
Jeff Reiner <theundeadghost@gmail.com>
2021-08-18 12:53:49 +0200

    Update connectAccount.tsx

--- components/connectAccount.tsx
+++ components/connectAccount.tsx
@@ -18,9 +18,18 @@ export default function ConnectAccount() {
       <div className="px-5 max-w-md mx-auto mb-4">
         <div className="bg-primary-400 rounded-xl ring-1 ring-inset ring-white ring-opacity-10 p-4">
           <div className="space-y-4">
-            <h1 className="text-lg font-semibold text-center">
-              Connect your Wallet
+            <h1 className="text-lg leading-none font-semibold text-center">
+              Sovreign Interface
             </h1>
+
+            <div className="space-y-2 text-gray-300">
+              <p>
+                This project is in Beta, while the smart contracts have been
+                audited, we can not guarantee full security.
+              </p>
+
+              <p>Please use at your own risk and discretion</p>
+            </div>
 
             <Button onClick={connect}>Connect Wallet</Button>
           </div>
