57f345782d55cdc30571233fca8f96867317c3ba
Jeff Reiner <theundeadghost@gmail.com>
2021-08-19 12:34:51 +0200

    Jazzy

--- components/identicon.tsx
+++ components/identicon.tsx
@@ -1,37 +1,16 @@
+import jazzicon from "jazzicon-ts";
 import { useEffect, useRef } from "react";
 
 export default function Identicon({ address }: { address: string }) {
-  const jazzicon = useRef<(diameter: number, seed: number) => HTMLDivElement>();
-
   const ref = useRef<HTMLDivElement>();
 
   useEffect(() => {
-    async function loadJazzicon() {
-      const generateIdenticon = (await import("@metamask/jazzicon")).default;
+    if (address && ref.current) {
+      ref.current.innerHTML = "";
 
-      jazzicon.current = generateIdenticon;
+      ref.current.appendChild(jazzicon(20, parseInt(address.slice(2, 10), 16)));
     }
-
-    loadJazzicon();
-  }, []);
-
-  useEffect(
-    () => {
-      if (typeof jazzicon.current === "undefined") {
-        return;
-      }
-
-      if (address && ref.current) {
-        ref.current.innerHTML = "";
-
-        ref.current.appendChild(
-          jazzicon.current(20, parseInt(address.slice(2, 10), 16))
-        );
-      }
-    },
-    /* eslint-disable-next-line react-hooks/exhaustive-deps */
-    [jazzicon.current, address]
-  );
+  }, [address]);
 
   return <div className="h-5 w-5 rounded-full bg-primary-400" ref={ref} />;
 }
