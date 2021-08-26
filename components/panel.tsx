90da6b624f43df527efbced97dd8fb3473cbafbf
Jeff Reiner <theundeadghost@gmail.com>
2021-08-26 12:01:19 +0200

    Update panel.tsx

--- components/panel.tsx
+++ components/panel.tsx
@@ -1,23 +1,26 @@
 import classNames from "classnames";
-import React from "react";
+import { forwardRef } from "react";
 
 type PanelProps = {
   light?: boolean;
   padding?: false | "p-1" | "p-2" | "p-3" | "p-4";
 } & JSX.IntrinsicElements["div"];
 
-export default function Panel({
-  light = false,
-  className = "",
-  padding = "p-4",
-  ...rest
-}: PanelProps) {
-  const cachedClassNames = classNames(
-    className,
-    padding,
-    "rounded-xl ring-1 ring-inset ring-white",
-    light ? "bg-primary-300 ring-opacity-20" : "bg-primary-400 ring-opacity-10"
-  );
+const Panel = forwardRef<HTMLDivElement, PanelProps>(
+  ({ light = false, className = "", padding = "p-4", ...rest }, ref) => {
+    const cachedClassNames = classNames(
+      className,
+      "rounded-xl ring-1 ring-inset ring-white",
+      light
+        ? "bg-primary-300 ring-opacity-20"
+        : "bg-primary-400 ring-opacity-10",
+      padding
+    );
 
-  return <div className={cachedClassNames} {...rest} />;
-}
+    return <div ref={ref} className={cachedClassNames} {...rest} />;
+  }
+);
+
+Panel.displayName = "Panel";
+
+export default Panel;
